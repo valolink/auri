42ae68ff5373cbe26f3b25e82c7eb9f714570925
Petteri <petteri.ketola@valolink.fi>
2025-05-13 14:10:06 +0300

    global vueAppData

--- src/useAppState.ts
+++ src/useAppState.ts
@@ -2,6 +2,24 @@ import { reactive } from 'vue'
 
 const settings = reactive(window.vueAppData?.settings || {})
 
+declare global {
+  interface Window {
+    vueAppData?: {
+      settings?: Record<string, AppSetting>
+    }
+  }
+
+  interface AppSetting {
+    label: string
+    type: 'number' | 'text' | 'select' | 'checkbox' | 'textarea'
+    sanitize: string
+    description: string
+    value: string | number | boolean
+    step?: string
+    options?: Record<string, string>
+  }
+}
+
 export function useAppState() {
   return {
     settings,
