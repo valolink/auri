53b26047f1a22cad28478645a35372911716da00
Petteri <petteri.ketola@valolink.fi>
2025-07-18 14:50:05 +0300

    tiltBoostFactor field

--- src/components/CalculationInputs.vue
+++ src/components/CalculationInputs.vue
@@ -138,6 +138,11 @@
             />
           </n-space>
         </n-form-item>
+
+        <n-form-item label="Kallistusvaikutus (%)">
+          <n-input-number v-model:value="settings.tiltBoostFactor.value" :step="0.01" @update:value="updateCalculationBasis(input.calculationBasis)" />
+        </n-form-item>
+
         <div style="display: flex; flex-wrap: wrap; gap: 8px">
           <n-button type="primary" :disabled="loading" @click="ajaxRequest('pdf_report')"
             >Tulosta raportti</n-button
