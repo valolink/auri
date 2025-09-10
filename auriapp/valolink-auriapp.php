b009624db100e1987bad6aa2b32239afcc5999e9
Reima Kokko <reima.kokko@valolink.fi>
2025-09-10 16:13:05 +0300

    chore(release): v0.1.8

--- auriapp/valolink-auriapp.php
+++ auriapp/valolink-auriapp.php
@@ -4,7 +4,7 @@ Plugin Name: Auriapp
 Plugin URI: valolink-auriapp
 Update URI: https://github.com/owner/repo
 Description:
-Version: 0.1.7
+Version: 0.1.8
 Author: Valolink
 Author URI: valolink.fi
 License:
